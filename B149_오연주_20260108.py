4c9998290281e867c06950a3bb21f4cc4326095b
unknown <wazoski05@gmail.com>
2026-01-08 17:26:45 +0900

    B149 - success

--- B149_오연주_20260108.py
+++ B149_오연주_20260108.py
@@ -1,14 +1,19 @@
-import sys
 from itertools import repeat
 
 count, length = map(int, input().split())
 word = ["" for _ in range(count)]
-seen = {}
 
 for i in range(count):
-    w = sys.stdin.readline().strip()
-    if(len(w)>=length):
-        seen[w] = seen.get(w,0) + 1
+    word[i] = input()
+
+seen = {}
+
+for w in word:
+    if len(w) >= length:
+        if w in seen:
+            seen[w] += 1
+        else:
+            seen[w] = 1
 
 for key, val in sorted(seen.items(),key=lambda x:(-x[1], -len(x[0]), x[0])):
     print(key)
