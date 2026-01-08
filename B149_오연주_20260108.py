8e0580f6d952d39c35a5afd37de6f7c758571fdf
unknown <wazoski05@gmail.com>
2026-01-08 17:26:08 +0900

    B149 - success (using sys.stdin)

--- B149_오연주_20260108.py
+++ B149_오연주_20260108.py
@@ -1,19 +1,14 @@
+import sys
 from itertools import repeat
 
 count, length = map(int, input().split())
 word = ["" for _ in range(count)]
-
-for i in range(count):
-    word[i] = input()
-
 seen = {}
 
-for w in word:
-    if len(w) >= length:
-        if w in seen:
-            seen[w] += 1
-        else:
-            seen[w] = 1
+for i in range(count):
+    w = sys.stdin.readline().strip()
+    if(len(w)>=length):
+        seen[w] = seen.get(w,0) + 1
 
 for key, val in sorted(seen.items(),key=lambda x:(-x[1], -len(x[0]), x[0])):
     print(key)
