fe0bcc795cdb5c889df6dc785cf8344be0fcb8a3
unknown <wazoski05@gmail.com>
2026-01-08 13:43:28 +0900

    A029 - success

--- A029_오연주_20260107.py
+++ A029_오연주_20260107.py
@@ -6,9 +6,8 @@ past = 0
 current = wayNum
 tmp = wayNum
 
-# print(current, wayNum, past)
-count = 1
-while(count != doorNum):
+
+for i in range(2,doorNum+1):
     if (tmp == 1):
         tmp = 0
         current = tmp
@@ -17,8 +16,23 @@ while(count != doorNum):
         tmp = 1
         current = tmp
         past = 0
+    if(i %3 == 0 and current != wayNum):
+        print("Love is open door")
+        exit()
+
+current = wayNum
+tmp = wayNum
+
+for _ in range(2, doorNum+1):
+    if(tmp == 1):
+        tmp = 0
+        current = tmp
+    else:
+        tmp = 1
+        current = tmp
+
     print(current)
-    count += 1
+
 
 
 
